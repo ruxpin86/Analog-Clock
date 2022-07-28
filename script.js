4cc2f32d456be344c2327c1f9ed63d5aa34e0603
Ted G <61029718+ruxpin86@users.noreply.github.com>
2022-07-28 09:36:51 -0600

    adding some styling to background

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function setDate() {
+  const secondHand = document.querySelector(".second-hand");
   const now = new Date();
   const seconds = now.getSeconds();
+  const secondsDegrees = (seconds / 60) * 360;
   console.log(seconds);
 }
 
