82ee548127769d5975c30fae5a5fe34a11a00ec6
Ted Glynn <will.glynn@gmail.com>
2022-07-29 13:55:21 -0600

    got hour and minute hand to transform properly

--- script.js
+++ script.js
@@ -9,12 +9,12 @@ function setDate() {
   const secondsDegrees = (seconds / 60) * 360 + 90;
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 
-  const mins = now.getMinutes();
-  const minsDegrees = (mins / 60) * 60 + 90;
-  minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+  const minutes = now.getMinutes();
+  const minsDegrees = (minutes / 60) * 360 + 90;
+  minuteHand.style.transform = `rotate(${minsDegrees}deg)`;
 
   const hour = now.getMinutes();
-  const hourDegrees = (hour / 12) * 60 + 90;
+  const hourDegrees = (hour / 12) * 360 + 90;
   hourHand.style.transform = `rotate(${hourDegrees}deg)`;
 
   console.log(seconds);
