7f93d88bae7880cdc3c06a1dbd0d8c08f66bb559
Ted G <61029718+ruxpin86@users.noreply.github.com>
2022-07-28 20:58:21 -0600

    minute hand function created like second hand

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const secondHand = document.querySelector(".second-hand");
+const minuteHand = document.querySelector(".min-hand");
+const hourHand = document.querySelector(".hour-hand");
 
 function setDate() {
   const now = new Date();
+
   const seconds = now.getSeconds();
   const secondsDegrees = (seconds / 60) * 360 + 90;
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
+  const mins = now.getMinutes();
+  const minDegrees = (mins / 60) * 60 + 90;
+  hourHand.style.transform = `rotate(${minDegrees}deg)`;
+
   console.log(seconds);
 }
 
